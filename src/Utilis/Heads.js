14112013a3086193579db369364b70f27703bb8f
Siam Ahnaf <siamahnaf198@gmail.com>
2022-01-22 01:58:18 +0600

    version@4.7.6 (Fixing Title Problem)

--- src/Utilis/Heads.js
+++ src/Utilis/Heads.js
@@ -4,7 +4,7 @@ const Heads = () => {
     return (
         <Head>
             <Head>
-                <meta property="og:url" content="https://www.codestation21.com/" />
+                <meta property="og:url" content="https://codestation21.com" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Code Station21 | Web and Mobile application developing agency" />
                 <meta property="og:description" content="We are web and mobile application developing agency . We use technology like React.js, Next.js, Express.js, Node.js, Restful API, GraphQL, MongoDB, Typrescript, Flutter and almost all javascript framework and libraray. We can solve design and develop applications fully coustomize for you. We provide complete digital space solutions for you." />
